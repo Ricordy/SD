64b9a9b2f56b85dff40b28443624299356af9a6a
Barrocz <youberrody@gmail.com>
2023-10-29 15:34:22 +0000

    changes

--- include/client_stub-private.h
+++ include/client_stub-private.h
@@ -3,10 +3,13 @@
 
 #include "client_stub.h"
 
-struct rtable_t {
+/* Remote table, que deve conter as informações necessária para comunicar*com o servidor. A definir pelo grupo em client_stub-private.h*/
+struct rtable_t
+{
     char *server_address;
     int server_port;
     int sockfd;
+    struct sockaddr_in socket;
 };
 
 #endif
