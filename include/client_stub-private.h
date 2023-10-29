723bcef91cd2f96fef307f2c4d95e93a2c9a4886
Barrocz <youberrody@gmail.com>
2023-10-29 15:35:22 +0000

    working client

--- include/client_stub-private.h
+++ include/client_stub-private.h
@@ -2,6 +2,7 @@
 #define _CLIENT_STUB_PRIVATE_H
 
 #include "client_stub.h"
+#include "inet.h"
 
 /* Remote table, que deve conter as informações necessária para comunicar*com o servidor. A definir pelo grupo em client_stub-private.h*/
 struct rtable_t
